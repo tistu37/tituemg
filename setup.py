89a3605ed4dc055efea7f9ac46a686b18f168e9f
Titu <104708899+tistu37@users.noreply.github.com>
2023-08-19 19:04:54 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+
+setuptools.setup(
+    name="tituemg",
+    version="0.0.1",
+    author="Tomas Tubino",
+    author_email="titubino@miuandes.cl.com",
+    description="Heramioentas utiles para el proceso de EMG",
+    url="https://github.com/tistu37/tituemg",
+    install_requires=['cipy', 'numpy'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
