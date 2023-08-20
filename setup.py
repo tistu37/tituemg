5c791ac8042cccc1c23c0b92516645b4516e22af
Titu <104708899+tistu37@users.noreply.github.com>
2023-08-20 16:46:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ import setuptools
 
 setuptools.setup(
     name="tituemg",
-    version="0.0.1.2",
+    version="0.0.1.3",
     author="Tomas Tubino",
     author_email="titubino@miuandes.cl.com",
     description="Heramioentas utiles para el proceso de EMG",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url="https://github.com/tistu37/tituemg",
-    install_requires=['cipy', 'numpy'],
+    install_requires=['scipy', 'numpy'],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
